97c59e886ad6b558e4cdd67c25d1a749937279be
Jaylo01 <jaylo@tech-noblade.com>
2026-05-11 07:54:45 +0800

    Clean up db migrations and fix receipt animation sync

--- db/migrations/2026_05_10_rollback_margins.sql
+++
@@ -1,9 +0,0 @@
-START TRANSACTION;
-
--- Revert price back to the original Codashop rate
-UPDATE product_skus SET price = cost_price;
-
--- Remove the cost_price column
-ALTER TABLE product_skus DROP COLUMN cost_price;
-
-COMMIT;
